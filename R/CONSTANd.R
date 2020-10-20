6cee02d32a6d56db38346bca338db158320e2f1e
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2020-10-20 12:09:14 +0200

    4 space indentation in CONSTANd.R

--- R/CONSTANd.R
+++ R/CONSTANd.R
@@ -14,42 +14,42 @@
 # Ported to R by Joris Van Houtven, 2020
 
 CONSTANd <- function(data, precision=1e-5, maxIterations=50, target=1){
-  # Return the normalized version of the input data (matrix) as an ndarray, as
-  # well as the convergence trail (residual error after each iteration) and the
-  # row and column multipliers R and S.
+    # Return the normalized version of the input data (matrix) as an ndarray, as
+    # well as the convergence trail (residual error after each iteration) and the
+    # row and column multipliers R and S.
 
-  Nrows = nrow(data)
-  Ncols = ncol(data)
+    Nrows = nrow(data)
+    Ncols = ncol(data)
 
-  convergence_trail = rep(NA, 2*maxIterations)
-  convergence = Inf
-  R = rep(1, Nrows)
-  S = rep(1, Ncols)
-  RM = rowMeans(data, na.rm = TRUE)
-  i = 0
-  while (precision<convergence && i<maxIterations) {
-    # fit the rows
-    tempR = target * 1/RM
-    data = data * tempR
-    R = R * tempR
-
-    CM = colMeans(data, na.rm = TRUE)
-    convergence_trail[2*i+1] = Nrows * sum(abs(CM - target), na.rm = TRUE) / 2
-
-    # fit the columns
-    tempS = target * 1/CM
-    data = t(t(data) * tempS)
-    S = S * tempS
-
+    convergence_trail = rep(NA, 2*maxIterations)
+    convergence = Inf
+    R = rep(1, Nrows)
+    S = rep(1, Ncols)
     RM = rowMeans(data, na.rm = TRUE)
-    convergence_trail[2*i+2] = Ncols * sum(abs(RM - target), na.rm = TRUE) / 2
+    i = 0
+    while (precision<convergence && i<maxIterations) {
+        # fit the rows
+        tempR = target * 1/RM
+        data = data * tempR
+        R = R * tempR
 
-    convergence = convergence_trail[2*i+2]
-    i = i+1
-  }
-  if (i == maxIterations) {
-    warning(cat(sprintf("Max number of CONSTANd iterations (%i) reached. Attained precision: %f.5", maxIterations, convergence)))
-  }
-  result = list("normalized_data" = data, "convergence_trail" = convergence_trail[seq(2*i)], "R" = R, "S" = S)
-  return(result)
+        CM = colMeans(data, na.rm = TRUE)
+        convergence_trail[2*i+1] = Nrows * sum(abs(CM - target), na.rm = TRUE) / 2
+
+        # fit the columns
+        tempS = target * 1/CM
+        data = t(t(data) * tempS)
+        S = S * tempS
+
+        RM = rowMeans(data, na.rm = TRUE)
+        convergence_trail[2*i+2] = Ncols * sum(abs(RM - target), na.rm = TRUE) / 2
+
+        convergence = convergence_trail[2*i+2]
+        i = i+1
+    }
+    if (i == maxIterations) {
+        warning(cat(sprintf("Max number of CONSTANd iterations (%i) reached. Attained precision: %f.5", maxIterations, convergence)))
+    }
+    result = list("normalized_data" = data, "convergence_trail" = convergence_trail[seq(2*i)], "R" = R, "S" = S)
+    return(result)
 }
